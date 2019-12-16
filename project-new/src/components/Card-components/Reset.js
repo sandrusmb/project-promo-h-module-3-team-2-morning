431f02fd4f9d442aa335b06e3eb57a694f65894d
Isabel del R <i.beldelreal@gmail.com>
2019-12-16 16:10:39 +0100

    Create Card and Reset Component. Reset is in Card Component folder

---
+++ project-new/src/components/Card-components/Reset.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// import '../stylesheets/App.scss';
+
+function Reset() {
+    return (
+        <div class="card__reset js-card-reset" title="Resetea los cambios">
+            <i class="far fa-trash-alt"></i>
+            Reset
+        </div>
+    );
+}
+export default Reset;
