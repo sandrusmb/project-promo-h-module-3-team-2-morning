539010089e4b08970f2494ec221bab2ef87bf961
sircferrer <ferrercarballo@gmail.com>
2019-12-17 11:56:15 +0100

    Add styles to reset btn and add branch dev and sassCard to project

--- project-new/src/components/Card-components/Reset.js
+++ project-new/src/components/Card-components/Reset.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from "react";
+import "../../stylesheets/Layout/Reset.scss";
 
 function Reset() {
-    return (
-        <div className="card__reset js-card-reset" title="Resetea los cambios">
-            <i className="far fa-trash-alt"></i>
-            Reset
-        </div>
-    );
+  return (
+    <div className="card__reset js-card-reset" title="Resetea los cambios">
+      <i className="far fa-trash-alt"></i>
+      Reset
+    </div>
+  );
 }
 export default Reset;
