b05bdc70d95981750cd94d27806b73506d2355fa
sircferrer <ferrercarballo@gmail.com>
2019-12-17 15:14:14 +0100

    Add default picture to photo.js

--- project-new/src/components/Card-components/Photo.js
+++ project-new/src/components/Card-components/Photo.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React from "react";
+import photoDefault from "../../images/photodefault.png";
 
 function Photo() {
-    return (
-        <div className="js__profile-image card__img-container"></div>
-    );
+  return (
+    <div className="js__profile-image card__img-container">
+      <img src={photoDefault} alt="default photo" title="photo" />
+    </div>
+  );
 }
 export default Photo;
