899bf49abfaca6a5104fbd9eb002557f7902925e
Isabel del R <i.beldelreal@gmail.com>
2019-12-17 15:56:17 +0100

    Create structure of form-components such as Collapsibles, Palettes

--- project-new/src/components/Form-components/Fill.js
+++ project-new/src/components/Form-components/Fill.js
@@ -1,45 +1,12 @@
 import React from 'react';
+import FillColllapsable from './FillCollapsable';
+import FillForm from './FillForm';
 
 function Fill() {
     return (
         <section className="collapsable-box fill hidden">
-            <header className="collapsable-box__header js-trigger">
-                <div> <i className="far fa-keyboard collapsable-box__icon"></i>
-
-                    <h2 className="collapsable-box__title">
-                        Rellena
-            </h2>
-
-                </div> <i className="fas fa-chevron-down arrow"></i>
-
-            </header>
-            <section className="collapsable-content js-content">
-                <label className="form__label" for="name">Nombre completo</label>
-                <input type="text" className="form__input js-form__input js-name" name="name" id="name"
-                    placeholder="Ej: Sally Jill" required value="" />
-                <label className="form__label" for="name">Puesto</label>
-                <input type="text" className="form__input js-form__input js-role" name="job" id="role"
-                    placeholder="Ej: Front-end unicorn" required value="" />
-                <label className="form__label" for="img">Imagen de perfil</label>
-                <div className="form__input--img">
-                    <input type="file" className="js__profile-upload-btn form__file" name="photo" id="img-selector"
-                        accept="image/*" />
-                    <button for=" file" className="js__profile-trigger form__file--label">Añadir imagen</button>
-                    <div className="js__profile-preview form__input--miniature"></div>
-                </div>
-                <label className="form__label" for="name">Email</label>
-                <input type="email" className="form__input js-form__input js-email" name="email" id="email"
-                    placeholder="Ej: sally-hill@gmail.com" required value="" />
-                <label className="form__label" for="name">Teléfono</label>
-                <input type="tel" className="form__input js-form__input js-phone" name="phone" id="phone"
-                    placeholder="Ej: 555-55-55-55" required value="" />
-                <label className="form__label" for="name">Linkedin</label>
-                <input type="text" className="form__input js-form__input js-linkedin" name="linkedin" id="linkedin"
-                    placeholder="Ej: sally.hill" required value="" />
-                <label className="form__label" for="name">Github</label>
-                <input type="text" className="form__input js-form__input js-github" name="github" id="github"
-                    placeholder="Ej: sally-hill" required value="" />
-            </section>
+            <FillColllapsable />
+            <FillForm />
         </section>
     );
 }
--- project-new/src/components/Form-components/Share.js
+++ project-new/src/components/Form-components/Share.js
@@ -1,35 +1,14 @@
 import React from 'react';
+import ShareCollapsable from './ShareCollapsable';
+import ShareContent from './ShareContent';
+
 
 function Share() {
     return (
         <section className="collapsable-box hidden">
-            <header className="collapsable-box__header js-trigger">
-                <div> <i className="fas fa-share-alt collapsable-box__icon"></i>
-
-                    <h2 className="collapsable-box__title">Comparte</h2>
-
-                </div> <i className="fas fa-chevron-down arrow"></i>
-
-            </header>
-            <section className="share__container collapsable-content js-content">
-                <button className="button"> <i className="far fa-address-card collapsable-box__icon"></i>
-
-                    <h2 className="button__title">Crear tarjeta</h2>
-
-                </button>
-                <div className="share__twitter hiddenButton"> <span className="share__twitter-message"> Tu tarjeta ha sido
-                creada: </span>
-
-                    <p className='share__twitter-url js-response'></p> <a target="_blank" href=""
-                        className="js-share__twitter share__twitter-button">
-                        <i className="fab fa-twitter"></i>
-                        Compartir en Twitter
-            </a>
-
-                </div>
-            </section>
+            <ShareCollapsable />
+            <ShareContent />
         </section>
     );
-
 }
 export default Share;
