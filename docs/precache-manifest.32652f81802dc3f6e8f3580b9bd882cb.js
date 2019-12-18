dd29d5506c6d1a6396c94ddb066b8b90a685a8bf
Isabel del R <i.beldelreal@gmail.com>
2019-12-18 11:21:29 +0100

    Fix project structure for docs

--- docs/precache-manifest.32652f81802dc3f6e8f3580b9bd882cb.js
+++ docs/precache-manifest.32652f81802dc3f6e8f3580b9bd882cb.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ada9fa3d0e35e53ed6f515b2625549b2",
+    "revision": "47063d68af73745a34497853b2d2ee1b",
     "url": "./index.html"
   },
   {
-    "revision": "eeed3c287b617e4b690f",
-    "url": "./static/css/main.fd08505f.chunk.css"
+    "revision": "cb7fa327bbfc1ad9b892",
+    "url": "./static/css/main.d58dac86.chunk.css"
   },
   {
     "revision": "e5eb6fd63b07f3a7af1a",
@@ -16,15 +16,23 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.9335742d.chunk.js.LICENSE"
   },
   {
-    "revision": "eeed3c287b617e4b690f",
-    "url": "./static/js/main.ee66507e.chunk.js"
+    "revision": "cb7fa327bbfc1ad9b892",
+    "url": "./static/js/main.e524c62b.chunk.js"
   },
   {
     "revision": "5b2f223308ceb1dfb2c1",
     "url": "./static/js/runtime-main.7fe83397.js"
   },
   {
+    "revision": "a82e2735ea190971b644f42c78489fb7",
+    "url": "./static/media/background.a82e2735.png"
+  },
+  {
     "revision": "ed8d747faa9998eb4e84cbbaf1c0539d",
     "url": "./static/media/logo-header.ed8d747f.png"
+  },
+  {
+    "revision": "157001de5c42442d979ed7d40f3fe2a4",
+    "url": "./static/media/photodefault.157001de.png"
   }
 ]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.33041dbf540bf2064b5414449da607b6.js"
+  "./precache-manifest.32652f81802dc3f6e8f3580b9bd882cb.js"
 );
 
 self.addEventListener('message', (event) => {
