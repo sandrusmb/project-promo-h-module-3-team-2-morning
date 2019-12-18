abfd4787ab0e756eb51fd0e837e7aa3e02bad4ec
sircferrer <ferrercarballo@gmail.com>
2019-12-18 12:06:32 +0100

    Add styles to icons card

--- src/components/Card-components/Icons.js
+++ src/components/Card-components/Icons.js
@@ -1,22 +1,22 @@
-import React from 'react';
+import React from "react";
+import "../../stylesheets/Sass-components/Icons.scss";
 
 function Icons() {
-    return (
-        <footer className="card__social">
-            <a className="card__icon js-card__icon js-card__icon--phone js-icon-deactivated" target="_blank"
-                title="Contacta por teléfono" href=""><i className="fas fa-mobile-alt"></i></a>
-
-            <a className="card__icon js-card__icon js-card__icon--email js-icon-deactivated" target="_blank"
-                title="Escríbeme por email" href=""><i className="far fa-envelope"></i>
-            </a> <a
-                className="card__icon js-card__icon js-card__icon--linkedin icon-deactivated js-icon-deactivated"
-                target="_blank" title="Añádeme a tu red de Linkedin" href=""><i
-                    className="fab fa-linkedin-in"></i></a>
-
-            <a className="card__icon js-card__icon js-card__icon--github js-icon-deactivated" target="_blank"
-                title="Visita mi portfolio de Github" href=""><i className="fab fa-github-alt"></i>
-            </a>
-        </footer>
-    );
+  return (
+    <footer className="card__social">
+      <a className="card__icon js-card__icon js-card__icon--phone js-icon-deactivated" target="_blank" title="Contacta por teléfono" href="">
+        <i className="fas fa-mobile-alt"></i>
+      </a>
+      <a className="card__icon js-card__icon js-card__icon--email js-icon-deactivated" target="_blank" title="Escríbeme por email" href="">
+        <i className="far fa-envelope"></i>
+      </a>{" "}
+      <a className="card__icon js-card__icon js-card__icon--linkedin icon-deactivated js-icon-deactivated" target="_blank" title="Añádeme a tu red de Linkedin" href="">
+        <i className="fab fa-linkedin-in"></i>
+      </a>
+      <a className="card__icon js-card__icon js-card__icon--github js-icon-deactivated" target="_blank" title="Visita mi portfolio de Github" href="">
+        <i className="fab fa-github-alt"></i>
+      </a>
+    </footer>
+  );
 }
 export default Icons;
