4946f411b552862b8ac1e0517c6a0f7ebd532f9c
marinadelafuente <mdelafuente18@gmail.com>
2019-12-26 17:03:49 +0100

    add icons json

--- src/components/Card-components/Icons.js
+++ src/components/Card-components/Icons.js
@@ -1,22 +1,26 @@
-import React from "react";
+import React from 'react';
 import "../../stylesheets/Sass-components/Icons.scss";
 
-function Icons() {
+function Icons(props) {
+  console.log(props.iconsApi)
   return (
     <footer className="card__social">
-      <a className="card__icon js-card__icon js-card__icon--phone js-icon-deactivated" target="_blank" title="Contacta por teléfono" href="">
-        <i className="fas fa-mobile-alt"></i>
-      </a>
-      <a className="card__icon js-card__icon js-card__icon--email js-icon-deactivated" target="_blank" title="Escríbeme por email" href="">
-        <i className="far fa-envelope"></i>
-      </a>{" "}
-      <a className="card__icon js-card__icon js-card__icon--linkedin icon-deactivated js-icon-deactivated" target="_blank" title="Añádeme a tu red de Linkedin" href="">
-        <i className="fab fa-linkedin-in"></i>
-      </a>
-      <a className="card__icon js-card__icon js-card__icon--github js-icon-deactivated" target="_blank" title="Visita mi portfolio de Github" href="">
-        <i className="fab fa-github-alt"></i>
-      </a>
+      {props.iconsApi.map((iconApi, index) => {
+        return <div>
+          <a
+            key={index}
+            className="card__icon js-card__icon js-card__icon--phone js-icon-deactivated"
+            target="_blank"
+            title={iconApi.title}
+            href=""></a>
+          <i className={props.iconApi.i}></i>
+        </div>
+      }
+      )}
     </footer>
+
   );
 }
+
+
 export default Icons;
--- src/components/Form.js
+++ src/components/Form.js
@@ -3,15 +3,26 @@ import Design from './Form-components/Design';
 import Fill from './Form-components/Fill';
 import Share from './Form-components/Share';
 import '../stylesheets/Sass-components/Form.scss';
+import iconsApi from '../api/IconsApi.json';
+import IconsCard from './Card-components/Icons';
 
-function Form() {
-    return (
-        <form className="form">
-            <Design />
-            <Fill />
-            <Share />
-        </form>
-    );
+class Form extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            iconsApi: iconsApi
+        }
+    }
+    render() {
+        return (
+            <form className="form">
+                <IconsCard iconsApi={this.state.iconsApi} />
+                <Design />
+                <Fill />
+                <Share />
+            </form>
+        );
+    }
 }
 
 export default Form;
