aee228b6943ca62f18305918bef1b1d5db56f6fe
sandrusmb <sandrusmb@gmail.com>
2020-01-07 12:03:28 +0100

    Fix color user and icons

--- src/components/Card-components/Icons.js
+++ src/components/Card-components/Icons.js
@@ -8,16 +8,29 @@ function Icons(props) {
   return (
     <footer className="card__social">
       {props.icons.map((icon, index) => {
-        return (
-          !props.formData[icon.name]
-            ? <span key={index} className="card__icon__deactivated" target="_blank" title={icon.title} > <i className={icon.i}></i></span>
-            :
-            <a key={index} className="icon__palette2" target="_blank" title={icon.title} href={icon.prefix + props.formData[icon.name]}>
-              <i className={icon.i}></i>
-            </a>
+        return !props.formData[icon.name] ? (
+          <span
+            key={index}
+            className="card__icon__deactivated"
+            target="_blank"
+            title={icon.title}
+          >
+            {" "}
+            <i className={icon.i}></i>
+          </span>
+        ) : (
+          <a
+            key={index} /* className="icon__palette2"  */
+            className={`icon__palette${palettes}`}
+            target="_blank"
+            title={icon.title}
+            href={icon.prefix + props.formData[icon.name]}
+          >
+            <i className={icon.i}></i>
+          </a>
         );
       })}
-    </footer >
+    </footer>
   );
 }
 
--- src/components/Card-components/User.js
+++ src/components/Card-components/User.js
@@ -11,10 +11,10 @@ class User extends React.Component {
     const job = this.props.formData.job;
     console.log(this.props.palettesData);
     const palettes = this.props.palettesData;
-
+    console.log(palettes);
     return (
       <div className="user-details">
-        <header className="user__palette2">
+        <header className={`user__palette${palettes}`}>
           <h1 className="card__name">{!name ? "Nombre Apellido" : name}</h1>
           <h2 className="card__role">{!job ? "Front-end Developer" : job}</h2>
         </header>
