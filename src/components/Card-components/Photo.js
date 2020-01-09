8f1dbd318db7bd1293973be69f995767862cf1e8
Isabel del R <i.beldelreal@gmail.com>
2020-01-09 15:31:31 +0100

    Add formData in Card Component

--- src/components/Card-components/Photo.js
+++ src/components/Card-components/Photo.js
@@ -3,8 +3,8 @@ import "../../stylesheets/Sass-components/Photo.scss";
 
 
 function Photo(props) {
-    console.log(props.formData.file);
-  return <div className="js__profile-image card__img-container"  style={{backgroundImage: `url(${props.formData.file || '/static/media/photodefault.157001de.png'})`}} >
+
+  return <div className="js__profile-image card__img-container" style={{ backgroundImage: `url(${props.formData.file || '/static/media/photodefault.157001de.png'})` }} >
   </div>;
 }
 export default Photo;
