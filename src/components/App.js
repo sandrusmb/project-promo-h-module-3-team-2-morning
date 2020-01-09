bae277759cd3947671ef07de676db5e8c831431f
sircferrer <ferrercarballo@gmail.com>
2020-01-09 16:19:56 +0100

    add react router to project

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "../stylesheets/App.scss";
+import { Route, Switch } from "react-router-dom";
 import Header from "./Header";
 import Card from "./Card";
 import Form from "./Form";
 import Footer from "./Footer";
+import HeaderLanding from "./HeaderLanding";
 
 class App extends React.Component {
   constructor(props) {
@@ -23,11 +25,9 @@ class App extends React.Component {
     this.resetData = this.resetData.bind(this);
   }
   handlePalette(data) {
-
     this.setState({ data });
   }
   handleInput(data) {
-
     this.setState({ [data.id]: data.inputValue });
   }
 
@@ -47,21 +47,22 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header />
-        <main className="main">
-          <Card
-            formData={this.state}
-            palettesData={this.state.data}
-            resetData={this.resetData}
+        <Switch>
+          <Route exact path="/" component={HeaderLanding} />
+          <Route
+            path="/card"
+            render={() => (
+              <>
+                <Header />
+                <main className="main">
+                  <Card formData={this.state} palettesData={this.state.data} resetData={this.resetData} />
+                  <Form handleInput={this.handleInput} handlePalette={this.handlePalette} file={this.state.file} formData={this.state} palettesData={this.state.data} />
+                </main>
+              </>
+            )}
           />
-          <Form
-            handleInput={this.handleInput}
-            handlePalette={this.handlePalette}
-            file={this.state.file}
-            formData={this.state}
-            palettesData={this.state.data}
-          />
-        </main>
+        </Switch>
+
         <Footer />
       </div>
     );
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,15 +1,16 @@
 import React from "react";
 import "../stylesheets/App.scss";
 import HeaderLanding from "./HeaderLanding";
+
 import Footer from "./Footer";
 
 function Landing() {
-    return (
-        <div>
-            <HeaderLanding />
-            <Footer />
-        </div>
-    );
+  return (
+    <div>
+      <HeaderLanding />
+      <Footer />
+    </div>
+  );
 }
 
 export default Landing;
