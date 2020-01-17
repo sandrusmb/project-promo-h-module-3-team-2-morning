b9d615291162854054f6d2aca2dc8a2be176625d
Isabel del R <i.beldelreal@gmail.com>
2020-01-17 02:29:29 +0100

    Fix error in photoDefault of Photo Component

--- src/components/Card-components/Photo.js
+++ src/components/Card-components/Photo.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "../../stylesheets/Sass-components/Photo.scss";
 import PropTypes from 'prop-types';
+import photoDefault from '../../images/photodefault.png';
 
 
 function Photo(props) {
 
-  return <div className="card__img-container" style={{ backgroundImage: `url(${props.formData.file || '/static/media/photodefault.157001de.png'})` }} >
+  return <div className="card__img-container" style={{ backgroundImage: `url(${props.formData.file || photoDefault})` }} >
   </div>;
 }
 
